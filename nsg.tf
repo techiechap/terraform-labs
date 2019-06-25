2a49981e1e445d108923b84a69b946fcc1ba73b1
AShish Ashish <ashish.specialist@gmail.com>
2019-06-25 13:44:44 -0500

    added network rule

--- nsg.tf
+++ nsg.tf
@@ -58,6 +58,20 @@ resource "azurerm_network_security_rule" "AllowSQLServer" {
     source_port_range           = "*"
     source_address_prefix       = "*"
 }
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowRDP"
+    resource_group_name         = "${azurerm_resource_group.nsgs.name}"
+    network_security_group_name = "${azurerm_network_security_group.resource_group_default.name}"
+
+    priority                    = 504
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 3389
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
 
 resource "azurerm_network_security_group" "nic_ubuntu" {
    name = "NIC_Ubuntu"
@@ -73,6 +87,23 @@ resource "azurerm_network_security_group" "nic_ubuntu" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = 22
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+  }
+  resource "azurerm_network_security_group" "nic_windows" {
+   name = "NIC_Windows"
+   resource_group_name  = "${azurerm_resource_group.nsgs.name}"
+   location             = "${azurerm_resource_group.nsgs.location}"
+   tags                 = "${azurerm_resource_group.nsgs.tags}"
+
+    security_rule {
+        name                       = "RDP"
+        priority                   = 506
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = 3389
         source_address_prefix      = "*"
         destination_address_prefix = "*"
   }
